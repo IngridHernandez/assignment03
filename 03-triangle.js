e73c434ac726f79338f66d97c2769caa6567e358
IngridHernandez <60713446+IngridHernandez@users.noreply.github.com>
2022-03-09 23:19:26 -0800

    Add files via upload

---
+++ 03-triangle.js
@@ -0,0 +1,19 @@
+/*
+var i;
+for (i = 1; i <= 10; i += 1) {
+  window.document.write(1 + "<br>");
+}
+
+for (var i = 1; i <= 11; i++) {
+
+};
+*/
+
+/*
+// Looping a triangel
+
+triangle = "#";
+for ( triangle = "#";  triangle <="#"; "#"++); {
+    window.document.write(triangle + "<br>");
+};
+*/
