cf0536430737dcae37949e2e36433bcd07f724b9
IngridHernandez <60713446+IngridHernandez@users.noreply.github.com>
2022-03-09 23:13:46 -0800

    Add files via upload

---
+++ 01-coinflip-redux.js
@@ -0,0 +1,13 @@
+
+// 1-Coin Flip Game variable
+var coinFlip;
+// 2-Loop
+for (coinFlip = 0; coinFlip <=10; coinflip++){
+    document.write( coinFlip + "<br>");
+// 3-randomly generated number
+var coinFlip = parseInt(window.prompt("Enter a number between 1and 10"));
+// 4-conditional statement
+if (coinFlip === 4) {
+    document.write("You guessed the right number");
+}
+}
---
+++ 02-coinflip-streak.js
@@ -0,0 +1,11 @@
+/*
+// 1-Coin Flip Game variable
+var coinFlip;
+
+// do while loop
+do {
+coinFlip +=1;
+window.document.write( + "br");
+} while(coinFlip < 5);
+window.document.write("You have reached the end");
+*/
